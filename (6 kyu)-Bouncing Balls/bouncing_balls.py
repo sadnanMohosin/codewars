0cb7bb848a1cae3018131f63b7dc3ac4c5f55b46
sadnanMohosin <83238537+sadnanMohosin@users.noreply.github.com>
2021-05-05 03:13:55 +0600

    Update bouncing_balls.py

--- (6 kyu)-Bouncing Balls/bouncing_balls.py
+++ (6 kyu)-Bouncing Balls/bouncing_balls.py
@@ -11,3 +11,10 @@ def bouncing_ball(h, bounce, window):
             count +=2
                 
     return count 
+
+### another exciting solution 
+
+def bouncingBall(h, bounce, window):
+    if h <= 0 or bounce <= 0 or bounce >= 1 or window >= h:
+        return -1
+    return 2 + bouncingBall(h * bounce, bounce, window)
