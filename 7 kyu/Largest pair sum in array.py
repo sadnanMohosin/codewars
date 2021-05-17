b0ee4745271414d66c269b37b5f9e51cb07feac4
sadnanMohosin <83238537+sadnanMohosin@users.noreply.github.com>
2021-05-17 19:41:19 +0600

    Update Largest pair sum in array.py

--- 7 kyu/Largest pair sum in array.py
+++ 7 kyu/Largest pair sum in array.py
@@ -5,3 +5,8 @@ def largest_pair_sum(numbers):
     numbers.sort()
     
     return sum(numbers[-2::])
+
+##
+def largest_pair_sum(numbers): 
+    # your code here
+    return sum(sorted(numbers)[-2:])
