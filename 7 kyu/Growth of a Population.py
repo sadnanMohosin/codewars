75e5b32f8092ae7fc0e7b44ed92a83387adc6450
sadnanMohosin <83238537+sadnanMohosin@users.noreply.github.com>
2021-05-23 15:15:41 +0600

    Create Growth of a Population.py

---
+++ 7 kyu/Growth of a Population.py
@@ -0,0 +1,13 @@
+def nb_year(p0, percent, aug, p):
+    # your code
+    
+    year = 0
+    
+    
+    
+    while p0 < p:
+        p0 = p0 + int(p0 * (percent/100)) + aug
+        year +=1
+        
+        
+    return year 
