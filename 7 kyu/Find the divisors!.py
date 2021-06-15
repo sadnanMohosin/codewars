8007962da59b7dbcdde7898d4209061f505ff35a
sadnanMohosin <83238537+sadnanMohosin@users.noreply.github.com>
2021-06-15 20:57:29 +0600

    Update Find the divisors!.py

--- 7 kyu/Find the divisors!.py
+++ 7 kyu/Find the divisors!.py
@@ -6,3 +6,10 @@ def divisors(integer):
     if div == []:
         return "{} is prime".format(integer)
     return div
+
+##another solution 
+
+
+def divisors(integer):
+    
+    return [i for i in range(2,integer) if integer%i==0] or f"{integer} is prime"
